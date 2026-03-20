ea22bfec8d62707e9fa18cc8d361e52e8b5354ce
Kawã <kawaklebersc@gmail.com>
2026-03-19 22:40:10 -0300

    add footer and header

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,21 @@
+import { FOOTER_LINKS } from "../../constants/footer"
+
 export function Footer() {
     return (
-        <div className="w-full h-16 bg-zinc-800 text-white flex items-center justify-center">
+        <div className="w-full h-16 bg-zinc-800 gap-1 text-white flex flex-col items-center justify-center">
             <h1 className="text-xl font-bold">Footer</h1>
+            <nav className="ml-4">
+                <ul className="flex gap-4">
+                    {FOOTER_LINKS.map((link) => (
+                        <li key={link.href}>
+                            <a href={link.href} className="hover:text-blue-500">
+                                {link.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </div>
     )
 }
+
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,9 +1,22 @@
-
+import { NAV_LINKS } from "../../constants/navigation"
 
 export function Header() {
     return (
         <div className="w-full h-16 bg-zinc-800 text-white flex items-center justify-center">
-            <h1 className="text-xl font-bold">Header</h1>
+            <div className="w-full px-10 flex items-center justify-between">
+                <h1 className="text-xl font-bold">Header</h1>
+                <nav className="ml-4">
+                    <ul className="flex gap-4">
+                        {NAV_LINKS.map((link) => (
+                            <li key={link.href}>
+                                <a href={link.href} className="hover:text-blue-500">
+                                    {link.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
         </div>
     )
 }
